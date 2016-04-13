947d4b875fb078a8a20422ca5974a7af6262cf2e
Dan <dvfranklin@gmail.com>
2016-04-13 15:12:13 -0400

    Add Joke and screenshot

---
+++ src/Joke.java
@@ -0,0 +1,9 @@
+
+public class Joke {
+    public static void main(String[] args){
+
+        System.out.println("What do you call cheese that isn't yours?");
+        System.out.println("Nacho cheese!");
+    }
+}
+
